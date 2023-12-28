c07dbefdb161be644c533dfdcf384f24b9b4e4b1
Medour Mehdi <mmedour@gmail.com>
2023-12-28 11:00:11 +0100

    Misc: m68000 makefile

---
+++ makefile000.mk
@@ -0,0 +1,54 @@
+_CC = m68k-atari-mint-g++
+SRC_DIR := ./
+OBJ_DIR := ./build
+BIN_DIR := ./bin
+
+LIB_XPDF := -lxpdf -lfofi -lgoo -lsplash 
+LIB_FREETYPE := -lfreetype -lbz2
+
+SRC := $(wildcard $(SRC_DIR)/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/zview/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/dither/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/nanosvg/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/qdbmp/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/rgb2lab/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/tgafunc/*.cpp) \
+  $(wildcard $(SRC_DIR)/*/flic/*.cpp)
+
+BIN := $(BIN_DIR)/mm_pic_68k.prg
+
+OBJ := $(SRC:$(SRC_DIR)/%.cpp=$(OBJ_DIR)/%.o)
+
+_CPPFLAGS := -I./
+
+_CFLAGS   := -m68000 -fomit-frame-pointer -fno-strict-aliasing -O2 
+
+_LDFLAGS  :=
+
+_LDLIBS   := -lgem -lpng -lz -lyuv -lheif -lwebp -lwebpdemux -ljpeg -ltiff -llzma -lzstd -lde265 -lx265 -lpthread -lgif $(LIB_XPDF) $(LIB_FREETYPE) -lpsd_malloc
+
+# _CFLAGS += -Wl,--stack,10485760
+
+.PHONY: all clean
+
+all: $(BIN)
+
+$(BIN): $(OBJ) | $(BIN_DIR)
+	$(_CC) $(_LDFLAGS) $^ $(_LDLIBS) -o $@
+	m68k-atari-mint-strip $(BIN)
+	
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp | $(OBJ_DIR)
+	@mkdir -p $(@D)
+	$(_CC) $(_CPPFLAGS) $(_CFLAGS) -c $< -o $@
+
+$(BIN_DIR) $(OBJ_DIR):
+	@mkdir -p $(@D)
+
+clean:
+	@$(RM) -rv $(BIN) $(OBJ_DIR)
+
+clean-objects:
+	@$(RM) -rv $(OBJ_DIR)
+
+-include $(OBJ:.o=.d)
