fba807302efd43e16d09b21f1fceb5f8da46c4f8
Medour Mehdi <mmedour@gmail.com>
2024-05-01 01:20:24 +0200

    Fix: fd_wdwidth value

--- utils/utils.cpp
+++ utils/utils.cpp
@@ -277,7 +277,7 @@ MFDB* mfdb_alloc_bpp( int8_t* buffer, int16_t width, int16_t height, int16_t bpp
     new_mfdb->fd_nplanes = bpp;
     new_mfdb->fd_w = width;
     new_mfdb->fd_h = height;
-    new_mfdb->fd_wdwidth = MFDB_STRIDE(width + width_stride) >> 4;
+    new_mfdb->fd_wdwidth = MFDB_STRIDE(width) >> 4;
 	new_mfdb->fd_stand = 0;
     new_mfdb->fd_r2 = 0;
     new_mfdb->fd_r3 = 0;
@@ -291,7 +291,7 @@ int16_t mfdb_update_bpp( MFDB* new_mfdb, int8_t* buffer, int16_t width, int16_t 
     new_mfdb->fd_nplanes = bpp;
     new_mfdb->fd_w = width;
     new_mfdb->fd_h = height;
-    new_mfdb->fd_wdwidth = MFDB_STRIDE(width + width_stride) >> 4;
+    new_mfdb->fd_wdwidth = MFDB_STRIDE(width) >> 4;
 	new_mfdb->fd_stand = 0;
     new_mfdb->fd_r2 = 0;    
     new_mfdb->fd_r3 = 0;
